79c7fe9303edd78502115ea7a9741206ee09971b
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2025-10-16 23:00:45 +0100

    added counter options to bnode map
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- lib/json/ld/node_identifier_map.ex
+++ lib/json/ld/node_identifier_map.ex
@@ -3,7 +3,11 @@ defmodule JSON.LD.NodeIdentifierMap do
   A virtual map over a pool of `ets` tables (one per online scheduler).
   Processes are mapped to a table by `phash2(self(), n_tables)` for low contention and good cache locality.
 
-  Every virtual map is isolated
+  Every virtual map is isolated. Each map has a counter that can use one of the following options
+
+  - `:system` -- System.unique_integer([monotonic, positive]) - the most scalable and concurrent, but shared, which might complicate matching bnodes when testing
+  - `:atomics` - in most cases faster / more concurrent that ets counters
+  - `:ets_counter` - the current implementation
 
   Usage:
 
@@ -11,13 +15,13 @@ defmodule JSON.LD.NodeIdentifierMap do
 
   use GenServer
 
-  # TODO add option to use the following options for counters:
-  # - System.unique_integer([monotonic, positive]) - the most scalable and concurrent, but shared, which might complicate matching bnodes when testing
-  # - :atomics - in most cases faster / more concurrent that ets counters
-  # - :ets (counters) - the current implementation
-  defstruct [:tab, :ref]
+  defstruct [:id, :tab, :counter_ref]
 
-  @type t :: %__MODULE__{tab: :ets.tid(), ref: reference()}
+  @type t :: %__MODULE__{
+          id: reference(),
+          tab: :ets.tid(),
+          counter_ref: :system | :ets_counter | {:atomics, reference()} | nil
+        }
 
   # persistent_term key
   @tables_key {__MODULE__, :tables}
@@ -32,47 +36,70 @@ defmodule JSON.LD.NodeIdentifierMap do
 
   @doc """
   Creates a Blank Node identifier map re-using a table from the pool.
+
+  ## Counter Option
+  Each map has a counter. The type of counter to be used can be configured using the application
+  environment option `blank_node_counter_type` or using the `counter_type` option when calling `new/1`.
+
+  The supported counter types are:
+  - `:ets_counter` — This is the default. The counter entry will be removed alongside the other map entries on `delete/1`. This is a zero-based counter.
+  - `:atomics` — Uses the `atomics` module. In most cases, this is faster and more concurrent than `ets` counters. Atomics are not tied to the current process and are automatically garbage collected when no longer referenced. This is a zero-based counter.
+  - `:system` — Uses `System.unique_integer([monotonic, positive])`. This is the most scalable and concurrent option. The only potential downside is that it is not a zero-based counter and it doesn't generate contiguous integers, which might complicate some use cases, particularly testing.
   """
   @spec new() :: t()
+  @spec new(Keyword.t()) :: t()
 
-  def new() do
+  def new(opts \\ []) do
+    counter_type =
+      Keyword.get_lazy(opts, :counter_type, fn ->
+        Application.get_env(:json_ld, :blank_node_counter_type, :ets_counter)
+      end)
+
+    unless counter_type in [:atomics, :ets_counter, :system] do
+      raise ArgumentError,
+        message:
+          "invalid counter_type option, " <>
+            "expected one of :atomics, :ets_counter or :system, got #{inspect(counter_type)}."
+    end
+
+    id = make_ref()
     tab = get_table()
-    ref = make_ref()
-    :ets.insert(tab, {{ref, :__counter__}, 0})
-    struct!(__MODULE__, tab: tab, ref: ref)
+    counter_ref = init_counter(id, tab, counter_type)
+    struct!(__MODULE__, id: id, tab: tab, counter_ref: counter_ref)
   end
 
   @doc """
-  Deletes the blank node identifier map (deleting all its entries from the ets table)
+  Deletes the blank node identifier map (deleting all its entries from `ets`).
   """
   @spec delete(t()) :: :ok
 
-  def delete(%__MODULE__{tab: tab, ref: ref}) do
-    _ = :ets.match_delete(tab, {{ref, :_}, :_})
+  def delete(%__MODULE__{id: id, tab: tab}) do
+    _ = :ets.match_delete(tab, {{id, :_}, :_})
     :ok
   end
 
   @doc """
-  Generate Blank Node Identifier
-
-  Details at <https://www.w3.org/TR/json-ld-api/#generate-blank-node-identifier>
+  Generate Blank Node Identifier.
+  Details at <https://www.w3.org/TR/json-ld-api/#generate-blank-node-identifier>.
   """
-  @spec generate_blank_node_id(t(), String.t() | nil) :: String.t()
+  @spec generate_blank_node_id(map :: t(), String.t() | nil) :: String.t()
 
   def generate_blank_node_id(map, identifier \\ nil)
 
-  def generate_blank_node_id(%__MODULE__{tab: tab, ref: ref}, nil) do
-    blank_node_id(tab, ref)
+  def generate_blank_node_id(%__MODULE__{} = map, nil) do
+    blank_node_id(map)
   end
 
-  def generate_blank_node_id(%__MODULE__{tab: tab, ref: ref}, identifier) do
-    case :ets.lookup(tab, {ref, identifier}) do
+  def generate_blank_node_id(%__MODULE__{id: id, tab: tab} = map, identifier) do
+    object = {id, identifier}
+
+    case :ets.lookup(tab, object) do
       [] ->
-        blank_node_id = blank_node_id(tab, ref)
-        :ets.insert(tab, {{ref, identifier}, blank_node_id})
+        blank_node_id = blank_node_id(map)
+        true = :ets.insert(tab, {object, blank_node_id})
         blank_node_id
 
-      [{{^ref, ^identifier}, blank_node_id}] ->
+      [{^object, blank_node_id}] ->
         blank_node_id
     end
   end
@@ -97,6 +124,7 @@ defmodule JSON.LD.NodeIdentifierMap do
       end
       |> List.to_tuple()
 
+    # Store tables in persistent_term for efficient access
     :persistent_term.put(@tables_key, %{tables: tables})
 
     {:ok, %{tables: tables}}
@@ -104,6 +132,7 @@ defmodule JSON.LD.NodeIdentifierMap do
 
   @impl true
   def terminate(_, %{tables: tables}) do
+    # if atomics was used it will be gc'ed when the process that owns this map dies
     _ = :persistent_term.erase(@tables_key)
     Enum.each(Tuple.to_list(tables), fn tab -> :ets.delete(tab) end)
     :ok
@@ -113,9 +142,7 @@ defmodule JSON.LD.NodeIdentifierMap do
   # PRIVATE
   # ================================================================================================
 
-  @doc """
-  Returns the ETS table identifier for an arbitrary pid (same mapping rule).
-  """
+  # Returns the ETS table identifier for an arbitrary pid (same mapping rule).
   @spec get_table(pid()) :: :ets.tid()
 
   defp get_table(pid \\ self()) when is_pid(pid) do
@@ -125,17 +152,37 @@ defmodule JSON.LD.NodeIdentifierMap do
     elem(tables, idx - 1)
   end
 
-  defp init_counter(tab, ref) do
-    :ets.insert(tab, {{ref, :__counter__}, 0})
+  defp init_counter(_, _, :system) do
+    :system
   end
 
-  defp incr_counter(tab, ref) do
-    :ets.update_counter(tab, {ref, :__counter__}, 1)
+  defp init_counter(_, _, :atomics) do
+    ref = :atomics.new(1, [{:signed, false}])
+    {:atomics, ref}
   end
 
-  defp blank_node_id(tab, ref) do
-    # Atomically increment and get the counter from ETS
-    counter = incr_counter(tab, ref)
+  defp init_counter(id, tab, :ets_counter) do
+    :ets.insert(tab, {{id, :__counter__}, 0})
+    :ets_counter
+  end
+
+  @spec incr_counter(t()) :: non_neg_integer()
+
+  defp(incr_counter(%__MODULE__{counter_ref: :system})) do
+    System.unique_integer([:positive, :monotonic])
+  end
+
+  defp incr_counter(%__MODULE__{counter_ref: :ets_counter} = map) do
+    :ets.update_counter(map.tab, {map.id, :__counter__}, 1)
+  end
+
+  defp incr_counter(%__MODULE__{counter_ref: {:atomics, ref}}) do
+    # atomics start at zero
+    :atomics.add_get(ref, 1, 1) - 1
+  end
+
+  defp blank_node_id(map) do
+    counter = incr_counter(map)
     "_:b#{counter - 1}"
   end
 end
